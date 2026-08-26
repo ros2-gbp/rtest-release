ecffd1b74586335f2a0d99e3716a53d8f660de06
Sławomir Cielepak <slawomir.cielepak@gmail.com>
2026-08-26 18:18:07 +0200

    Rebase from 'release/lyrical/rtest'

--- include/lyrical/rtest/action_server_base.hpp
+++ include/lyrical/rtest/action_server_base.hpp
@@ -52,7 +52,7 @@ public:
     Expired,
   };
   ServerBase() = default;
-  virtual ~ServerBase() = default;
+  ~ServerBase() override = default;
 
   virtual void publish_status() {}
   virtual void notify_goal_terminal_state() {}
